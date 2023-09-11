6e7c8fcfbb6b129cc5ce45d4bbae53db3bdd053b
ReliabilityEng <isaacmortera@gmail.com>
2023-09-11 20:54:38 +1200

    Add a for loop in generateGrid to create horizontal and vertical divs to act as pixels

--- script.js
+++ script.js
@@ -3,15 +3,19 @@ const containerDiv = document.querySelector('.container');
 
 
 // Variables
-let gridResolution = 16;    // This is 1:1 Aspect Ratio
+let gridResolution = 4;    // This is 1:1 Aspect Ratio
 
 function generateGrid(gridResolution) {
     // Generates a square grid with a vertical and horizontal resolution of gridResolution
     for(let horizontalPixel = 0; horizontalPixel < gridResolution; horizontalPixel++) {
+            const pixelDivRow = document.createElement('div');
+            pixelDivRow.classList.add('pixelDivRow');
+            containerDiv.appendChild(pixelDivRow);
+
         for(let verticalPixel = 0; verticalPixel < gridResolution; verticalPixel++) {
-            const pixelDiv = document.createElement('div');
-            pixelDiv.classList.add('pixelDiv');
-            containerDiv.appendChild(pixelDiv);
+            const pixelDivCol = document.createElement('div');
+            pixelDivCol.classList.add('pixelDivColumn');
+            pixelDivRow.appendChild(pixelDivCol);
         }
     }
 }

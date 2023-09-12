3e4c7f676aa8ff1784fb2b4ccc1d8d21c3e9c537
ReliabilityEng <isaacmortera@gmail.com>
2023-09-12 22:02:49 +1200

    Create a mouseDown event listener using mousedown and mouseup This is to imitate a mousehold event which does not exist in vanilla JS. Need the mousehold event to draw over the grid

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 // Creation of Elements
 const containerDiv = document.querySelector('.container');
 
+// Mouse hold down event
+let mouseDown;
+window.addEventListener('mousedown',(evt) => mouseDown = true);
+window.addEventListener('mouseup',(evt) => mouseDown = false);
+
 
 // Variables
-let gridResolution = 4;    // This is 1:1 Aspect Ratio
+let gridResolution = 32;    // This is 1:1 Aspect Ratio
 
 function generateGrid(gridResolution) {
     // Generates a square grid with a vertical and horizontal resolution of gridResolution
-    for(let horizontalPixel = 0; horizontalPixel < gridResolution; horizontalPixel++) {
+    for(let rowContainer = 0; rowContainer < gridResolution; rowContainer++) {
             const pixelDivRow = document.createElement('div');
             pixelDivRow.classList.add('pixelDivRow');
             containerDiv.appendChild(pixelDivRow);
 
-        for(let verticalPixel = 0; verticalPixel < gridResolution; verticalPixel++) {
+        for(let columnPixel = 0; columnPixel < gridResolution; columnPixel++) {
             const pixelDivCol = document.createElement('div');
             pixelDivCol.classList.add('pixelDivColumn');
             pixelDivRow.appendChild(pixelDivCol);
@@ -20,4 +25,39 @@ function generateGrid(gridResolution) {
     }
 }
 
+function addHoverEffect() {
+    const pixels = document.querySelectorAll('.pixelDivColumn');
+    pixels.forEach((pixel) => pixel.addEventListener('mouseover', 
+    () => {
+        if(mouseDown) {
+            pixel.classList.add('coloredPixel');
+        }        
+    }
+    ));
+    
+}
+
 generateGrid(gridResolution);
+addHoverEffect();
+
+
+
+// isDown = false;
+// const DURATION = 500
+// elem = document.querySelector('body');
+// elem.addEventListener('mousedown', function(event) { 
+// 	// simulating hold event
+//     if (isDown == false){
+// 		isDown = true;
+// 	    setInterval(function() {
+// 			if (isDown == true){
+// 				console.log("Hold");
+//                 console.log(event.target)
+// 			}
+// 		}, DURATION);
+// 	}
+// });
+
+// elem.addEventListener('mouseup', function(event) {
+// 	isDown=false;  
+// });
